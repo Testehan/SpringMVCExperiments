cc76eea9def81a6bf5dc3f430ac9329bbc239dc4
DanTestehan <tdan89@yahoo.com>
2023-05-22 17:45:11 +0300

    new entities and DTO for login register

---
+++ src/main/java/com/testehan/SpringMVCExperiments/dto/UserRegistrationDTO.java
@@ -0,0 +1,24 @@
+package com.testehan.SpringMVCExperiments.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegistrationDTO {
+
+    private Long id;
+    @NotEmpty(message = "Username should not be empty")
+    private String username;
+    @NotEmpty(message = "Email should not be empty")
+    private String email;
+    @NotEmpty(message = "Password should not be empty")
+    private String password;
+
+
+}
---
+++ src/main/java/com/testehan/SpringMVCExperiments/model/Role.java
@@ -0,0 +1,28 @@
+package com.testehan.SpringMVCExperiments.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<UserEntity> roles = new ArrayList<>();
+
+}
